b3cc796ee8f69b459801e6f1c7270bdd9aa6428f
Carlos <crrlos1801@gmail.com>
2015-11-05 11:09:09 -0600

    creada la estructura básica del socket servidor

--- ChessSocketServer/src/chesssocketserver/ChessSocketServer.java
+++ ChessSocketServer/src/chesssocketserver/ChessSocketServer.java
@@ -1,21 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package chesssocketserver;
 
-/**
- *
- * @author Carlos
- */
-public class ChessSocketServer {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-    /**
-     * @param args the command line arguments
-     */
+
+public class ChessSocketServer {
+   ServerSocket serverSocket;
+   Socket jugadorSocket;
+    public ChessSocketServer() {
+       try {
+           serverSocket = new ServerSocket(40000);
+       } catch (IOException ex) {
+           Logger.getLogger(ChessSocketServer.class.getName()).log(Level.SEVERE, null, ex);
+       }
+    }
+    public void start(){
+        while(true){
+            try {
+                jugadorSocket = serverSocket.accept();
+            } catch (IOException ex) {
+                Logger.getLogger(ChessSocketServer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        ChessSocketServer socketServer = new ChessSocketServer();
+        socketServer.start();
+       
     }
     
 }
