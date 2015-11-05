7827630ba2cf09f06d6c0164206df019ba5f214c
Carlos <crrlos1801@gmail.com>
2015-11-05 11:30:58 -0600

    agregada función buscar y variables para la entrada y salida del Stream

--- ChessSocketServer/src/chesssocketserver/ChessSocketServer.java
+++ ChessSocketServer/src/chesssocketserver/ChessSocketServer.java
@@ -1,37 +1,50 @@
-
 package chesssocketserver;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class ChessSocketServer {
-   ServerSocket serverSocket;
-   Socket jugadorSocket;
+
+    ServerSocket serverSocket;
+    Socket jugadorSocket;
+    DataInputStream in;
+    DataOutputStream out;
+
     public ChessSocketServer() {
-       try {
-           serverSocket = new ServerSocket(40000);
-       } catch (IOException ex) {
-           Logger.getLogger(ChessSocketServer.class.getName()).log(Level.SEVERE, null, ex);
-       }
+        try {
+            serverSocket = new ServerSocket(40000);
+        } catch (IOException ex) {
+            Logger.getLogger(ChessSocketServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    public void start(){
-        while(true){
+
+    public void start() {
+        while (true) {
             try {
                 jugadorSocket = serverSocket.accept();
+                in = new DataInputStream(jugadorSocket.getInputStream());
+
             } catch (IOException ex) {
                 Logger.getLogger(ChessSocketServer.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
-   
+
+    private void buscar(String nombreJugador) {
+        for (Jugador j : Jugador.getJugadores()) {
+
+        }
+    }
+
     public static void main(String[] args) {
         ChessSocketServer socketServer = new ChessSocketServer();
         socketServer.start();
-       
+
     }
-    
+
 }
