1cc36bf9b5c2f0f54f3dbdf56597724ed3c860ad
Carlos <crrlos1801@gmail.com>
2015-11-05 13:29:22 -0600

    agregadas funciones para la interaccion entre jugadores y un bucle while para mantener el hilo a la escucha de su cliente

--- ChessSocketServer/src/chesssocketserver/Jugador.java
+++ ChessSocketServer/src/chesssocketserver/Jugador.java
@@ -17,6 +17,8 @@ package chesssocketserver;
 
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class Jugador extends Thread{
@@ -29,8 +31,25 @@ public class Jugador extends Thread{
     @Override
     public void run() {
         super.run(); 
+        while(true){
+            try {
+                Thread.sleep(250);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Jugador.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            
+        }
     }
-
+    
+    private void invitarJugador(String nombre){
+    
+        
+    }
+    private void enviarInvitacion(){
+    
+    }
+    
     
     public static ArrayList<Jugador> getJugadores() {
         return jugadores;
