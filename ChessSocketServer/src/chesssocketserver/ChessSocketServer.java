c2f743d710612aba25fe47904cfe07204784b0aa
Carlos <crrlos1801@gmail.com>
2015-11-11 23:37:16 -0600

    commit

--- ChessSocketServer/src/chesssocketserver/ChessSocketServer.java
+++ ChessSocketServer/src/chesssocketserver/ChessSocketServer.java
@@ -5,16 +5,18 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.cert.CertificateFactory;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ChessSocketServer {
-    
+
     ServerSocket serverSocket;
     Socket jugadorSocket;
     DataInputStream in;
     DataOutputStream out;
-    
+
     public ChessSocketServer() {
         try {
             serverSocket = new ServerSocket(40000);
@@ -22,44 +24,74 @@ public class ChessSocketServer {
             Logger.getLogger(ChessSocketServer.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     public void start() {
         while (true) {
             try {
                 jugadorSocket = serverSocket.accept();
                 in = new DataInputStream(jugadorSocket.getInputStream());
-                
+
                 String nombre = in.readUTF();
-                
-                if(!buscarActualizar(nombre)){
-                    
-                Jugador jugador = new Jugador();
-                jugador.setJugadorOrigen(jugadorSocket);
-                jugador.setNombreJugador(nombre);
-                jugador.start();
-                Jugador.jugadores.add(jugador);
-                
+
+                if (!buscarActualizar(nombre)) {
+
+                    Jugador jugador = new Jugador();
+                    jugador.setJugadorOrigen(jugadorSocket);
+                    jugador.setNombreJugador(nombre);
+                    jugador.start();
+                    Jugador.jugadores.add(jugador);
+
                 }
             } catch (IOException ex) {
                 Logger.getLogger(ChessSocketServer.class.getName()).log(Level.SEVERE, null, ex);
             }
-           }
+
+            for (Jugador j : Jugador.jugadores) {
+
+                System.out.println(j.getNombreJugador());
+
+            }
+            System.out.println(jugadorSocket);
+            //break;
+
+        }
+
     }
-    
+
     private boolean buscarActualizar(String nombreJugador) {
         for (Jugador j : Jugador.getJugadores()) {
             if (j.getNombreJugador().equals(nombreJugador)) {
-                j.setJugadorOrigen(jugadorSocket);
+                
+                Jugador jugador = new Jugador();
+                jugador.setJugadorDestino(j.getJugadorDestino());
+                jugador.setTiempo(j.getTiempo());
+                jugador.setJugadorOrigen(jugadorSocket);
+                jugador.start();
+                j = jugador;
+                
                 return true;
             }
         }
         return false;
     }
     
+    public void test(String movimiento){
+        try {
+            DataOutputStream out = new DataOutputStream(jugadorSocket.getOutputStream());
+            out.writeUTF(movimiento);
+        } catch (IOException ex) {
+            Logger.getLogger(ChessSocketServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         ChessSocketServer socketServer = new ChessSocketServer();
         socketServer.start();
-        
+        while(true){
+            socketServer.test(s.nextLine());
+        }
+
     }
-    
+
 }
